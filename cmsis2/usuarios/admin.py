3f2289ef1575933caba07554f9a04931bd2b2b5a
frandepy2 <francisco.sanabria@somosetica.com>
2023-08-22 21:45:15 -0400

    Agrega subscribir al usuario

--- cmsis2/usuarios/admin.py
+++ cmsis2/usuarios/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Usuario
 
 # Register your models here.
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'date_joined', 'subscribed')
+    list_filter = ('is_staff', 'is_active', 'date_joined')
+    search_fields = ('username', 'email')
+
+admin.site.register(Usuario,UsuarioAdmin)
--- cmsis2/usuarios/manager.py
+++ cmsis2/usuarios/manager.py
@@ -1,16 +1,8 @@
 from django.contrib.auth.base_user import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('Cada usuario debe tener un email')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_admin(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
