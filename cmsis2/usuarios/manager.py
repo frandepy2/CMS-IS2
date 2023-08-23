92a664198e6b3babd1d8740fd2ee0a07856ea3ef
frandepy2 <francisco.sanabria@somosetica.com>
2023-08-22 23:43:38 -0400

    Corrige el manager de usuarios y crea el test de creacion de usuarios

--- cmsis2/usuarios/manager.py
+++ cmsis2/usuarios/manager.py
@@ -4,7 +4,7 @@ class CustomUserManager(BaseUserManager):
 
     def create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('Cada usuario debe tener un email')
+            raise ValueError('Email obligatorio')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -13,7 +13,7 @@ class CustomUserManager(BaseUserManager):
 
     def create_admin(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
--- cmsis2/usuarios/tests.py
+++ cmsis2/usuarios/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UsuarioTestCase(TestCase):
+    def test_crear_usuario(self):
+        Usuario = get_user_model()
+        user = Usuario.objects.create_user(email = 'usuario@gmail.com', password='password')
+        self.assertEquals(user.email,'usuario@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        with self.assertRaises(TypeError):
+            Usuario.objects.create_user()
+
+    def test_usuario_subscriptor(self):
+        Usuario = get_user_model()
+        user = Usuario.objects.create_user(email = 'usuario@gmail.com', password='password')
+        self.assertTrue(user.subscribed)
+
+    def test_crear_administrador(self):
+        Usuario = get_user_model()
+        user = Usuario.objects.create_admin(email = 'usuario@gmail.com', password='password')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+    def test_primer_usuario_admin(self):
+        Usuario = get_user_model()
+        Usuario.objects.all().delete()
+        self.assertEqual(Usuario.objects.all().count(), 0,
+        "No se pudo limpiar la base de datos")
+        Usuario.objects.create_user(email='usuario@gmail.com', password='password')
+        self.assertEqual(Usuario.objects.filter(
+            groups__name='cms_admin').count(), 1)
