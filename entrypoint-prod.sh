1a9094ddd2d184678be173fa746fbbfb4867ca46
frandepy2 <frandepy@fpuna.edu.py>
2023-08-23 23:13:38 -0400

    Corrige archivos para enviar a produccion

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 sleep 5
-
+echo "Inicia el proceso de hacer migraciones"
+python manage.py makemigrations usuarios
+echo "finaliza el proceso de hacer migraciones"
 python manage.py migrate --no-input
 python manage.py collectstatic --noinput
 
