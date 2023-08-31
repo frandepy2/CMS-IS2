065a70cab55182ac1961e5d11a8164c68580361b
Jovana <jovananayeli@gmail.com>
2023-08-30 21:53:39 -0400

    Cambios en archivos admin y settings

--- cmsis2/roles/admin.py
+++ cmsis2/roles/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import CustomRole, CustomPermission
+from .models import CustomRole, CustomPermission, RolePermission, UserCategoryRole
 
 # Register your models here.
 
 admin.site.register(CustomRole)
 admin.site.register(CustomPermission)
+admin.site.register(RolePermission)
+admin.site.register(UserCategoryRole)
