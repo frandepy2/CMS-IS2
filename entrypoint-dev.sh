afe30e1c1930ce6e32a2c6c5a111b250c7072cfc
frandepy2 <francisco.sanabria@somosetica.com>
2023-08-31 00:36:28 -0400

    agrega metodo para poblar la base de datos automaticamente

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -5,6 +5,9 @@ echo "Inicia el proceso de hacer migraciones"
 python manage.py makemigrations usuarios roles categorias
 echo "finaliza el proceso de hacer migraciones"
 python manage.py migrate --no-input
+echo "Inicia el proceso de guardar la base de datos"
+python manage.py shell < load_database.py
+echo "finaliza el proceso de guardar la base de datos"
 python manage.py collectstatic --noinput
 
 
