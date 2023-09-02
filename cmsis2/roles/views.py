19b816671c8bc50597f927c2c396ea591d558bb9
Jovana <jovananayeli@gmail.com>
2023-09-02 11:02:24 -0400

    Agrega decorators para verificar los permisos en views

--- cmsis2/roles/views.py
+++ cmsis2/roles/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CustomRoleForm
 from .models import CustomRole
 from django.core.paginator import Paginator, Page
+from django.contrib.auth.decorators import login_required
+from .decorators import has_permission_decorator, has_category_permission_decorator
+
+
 
 # Create your views here.
+@login_required
+@has_permission_decorator('view_roles')
 def roles(request):
     roles_list = CustomRole.objects.all().order_by('id')  # Obtén todos los roles
     paginator = Paginator(roles_list,5)
@@ -13,6 +19,9 @@ def roles(request):
 
     return render(request, 'roles/roles.html', {'page': page})
 
+
+@login_required
+@has_permission_decorator('create_roles')
 def crear_rol(request):
     if request.method == 'POST':
         form = CustomRoleForm(request.POST)
@@ -23,10 +32,16 @@ def crear_rol(request):
         form = CustomRoleForm()
     return render(request, 'roles/crear_rol.html', {'form': form})
 
+
+@login_required
+@has_permission_decorator('view_roles')
 def mas_informacion_rol(request, role_id):
     role = CustomRole.objects.get(id=role_id)
     return render(request, 'roles/more_info.html', {'role': role})
 
+
+@login_required
+@has_permission_decorator('edit_roles')
 def editar_rol(request,role_id):
     role = get_object_or_404(CustomRole, id=role_id)
 
