7b3655233d138a9d3dc7da938457f261db51b1fd
frandepy2 <francisco.sanabria@somosetica.com>
2023-09-02 12:58:56 -0400

    Agrega primeras pantallas de gestion de usuario

--- cmsis2/usuarios/urls.py
+++ cmsis2/usuarios/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.usuarios, name='usuarios'),
+    path('manage/<int:user_id>/',views.manage_user, name='manage_user'),
 ]
--- cmsis2/usuarios/views.py
+++ cmsis2/usuarios/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import Usuario
+from roles.models import CustomRole, UserCategoryRole
+from django.core.paginator import Paginator, Page
 
 # Create your views here.
 def home(request):
     return render(request, 'usuarios/home.html')
+
+def usuarios(request):
+    usuario_list = Usuario.objects.all().order_by('id')  # Obtén todos los roles
+    paginator = Paginator(usuario_list,5)
+
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
+    return render(request, 'usuarios/usuarios.html', {'page': page})
+
+def manage_user(request, user_id):
+    user = Usuario.objects.get(id=user_id)
+    user_roles = UserCategoryRole.objects.filter(user=user)
+    return render(request, 'usuarios/manage_user.html', {'user': user, 'roles':user_roles})
