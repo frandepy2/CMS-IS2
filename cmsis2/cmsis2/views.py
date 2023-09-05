5722cfb04e073f4bfdcc00c797a69ac63cac89cb
Jovana <jovananayeli@gmail.com>
2023-09-05 15:02:56 -0400

    Lista categorias en sidebar

--- cmsis2/cmsis2/views.py
+++ cmsis2/cmsis2/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from categorias.models import Categoria
 
 
 def HomeView(request):
     page_title = "Ultimas Entradas"
-    return render(request, 'home/home.html', {'page_title': page_title})
+    categorias = Categoria.objects.filter(is_active=True)
+    #categorias = Categoria.objects.all()
+    return render(request, 'home/home.html', {'page_title': page_title, 'categorias': categorias})
