96090e8614a9e411040a98ebb760bcb640d19dd0
Jovana <jovananayeli@gmail.com>
2023-09-06 17:13:49 -0400

    Comentarios

--- cmsis2/decorators.py
+++ cmsis2/decorators.py
@@ -6,6 +6,14 @@ from roles.models import UserCategoryRole
 
 
 def has_permission_decorator(permission_name):
+    """
+    Decorador que verifica si un usuario tiene un permiso específico.
+
+    :param permission_name: El nombre del permiso que se debe verificar.
+    :type permission_name: str
+    :return: La función de vista envuelta si el usuario tiene el permiso, de lo contrario, redirecciona.
+    :rtype: callable
+    """
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
@@ -13,7 +21,7 @@ def has_permission_decorator(permission_name):
             if has_permission(user, permission_name):
                 return view_func(request, *args, **kwargs)
             else:
-                # Redirect to the previous page
+                # Redirige a la página anterior si el usuario no tiene el permiso.
                 previous_url = request.META.get('HTTP_REFERER', reverse('home'))
                 return HttpResponseRedirect(previous_url)
         return _wrapped_view
@@ -21,6 +29,16 @@ def has_permission_decorator(permission_name):
 
 
 def has_category_permission_decorator(category_id, permission_name):
+    """
+    Decorador que verifica si un usuario tiene un permiso específico de categoría.
+
+    :param category_id: El ID de la categoría para la que se debe verificar el permiso.
+    :type category_id: int
+    :param permission_name: El nombre del permiso que se debe verificar.
+    :type permission_name: str
+    :return: La función de vista envuelta si el usuario tiene el permiso de categoría, de lo contrario, retorna una respuesta prohibida.
+    :rtype: callable
+    """
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
@@ -34,6 +52,16 @@ def has_category_permission_decorator(category_id, permission_name):
 
 
 def has_permission(user, permission_name):
+    """
+    Verifica si un usuario tiene un permiso específico.
+
+    :param user: El usuario cuyo permiso se debe verificar.
+    :type user: User (o el tipo de usuario correspondiente en tu modelo de datos)
+    :param permission_name: El nombre del permiso que se debe verificar.
+    :type permission_name: str
+    :return: True si el usuario tiene el permiso, False en caso contrario.
+    :rtype: bool
+    """
     try:
         return UserCategoryRole.objects.filter(
             user=user,  # Filtra por usuario
@@ -45,6 +73,18 @@ def has_permission(user, permission_name):
 
 
 def has_category_permission(user, category_id, permission_name):
+    """
+    Verifica si un usuario tiene un permiso específico dentro de una categoría.
+
+    :param user: El usuario cuyo permiso de categoría se debe verificar.
+    :type user: User (o el tipo de usuario correspondiente en tu modelo de datos)
+    :param category_id: El ID de la categoría para la que se debe verificar el permiso.
+    :type category_id: int
+    :param permission_name: El nombre del permiso que se debe verificar.
+    :type permission_name: str
+    :return: True si el usuario tiene el permiso de categoría, False en caso contrario.
+    :rtype: bool
+    """
     try:
         user_category_role = UserCategoryRole.objects.get(user=user, category_id=category_id)
         return user_category_role.role.permissions.filter(name=permission_name).exists()
--- cmsis2/usuarios/views.py
+++ cmsis2/usuarios/views.py
@@ -11,6 +11,16 @@ from decorators import has_permission_decorator
 @login_required
 @has_permission_decorator('view_users')
 def usuarios(request):
+    """
+    Vista para mostrar la lista de usuarios.
+
+    Esta vista muestra una lista paginada de usuarios y requiere que el usuario tenga el permiso 'view_users' para acceder.
+
+    :param request: La solicitud HTTP.
+    :type request: HttpRequest
+    :return: La página de usuarios paginada.
+    :rtype: HttpResponse
+    """
     page_title = 'Usuarios'
     usuario_list = Usuario.objects.all().order_by('id')  # Obtén todos los roles
     paginator = Paginator(usuario_list,5)
@@ -26,6 +36,18 @@ def usuarios(request):
 @login_required
 @has_permission_decorator('asign_roles')
 def manage_user(request, user_id):
+    """
+    Vista para gestionar un usuario específico.
+
+    Permite a los usuarios con el permiso 'asign_roles' gestionar roles asignados a un usuario en particular.
+
+    :param request: La solicitud HTTP.
+    :type request: HttpRequest
+    :param user_id: El ID del usuario que se va a gestionar.
+    :type user_id: int
+    :return: La página de gestión de usuario.
+    :rtype: HttpResponse
+    """
     user = Usuario.objects.get(id=user_id)
     user_roles = UserCategoryRole.objects.filter(user=user)
     page_title = user.email
@@ -39,6 +61,18 @@ def manage_user(request, user_id):
 @login_required
 @has_permission_decorator('asign_roles')
 def asignar_rol(request, user_id):
+    """
+    Vista para asignar un rol a un usuario.
+
+    Permite a los usuarios con el permiso 'asign_roles' asignar un rol a un usuario específico.
+
+    :param request: La solicitud HTTP.
+    :type request: HttpRequest
+    :param user_id: El ID del usuario al que se le asignará el rol.
+    :type user_id: int
+    :return: El formulario de asignación de roles o la página de usuarios después de la asignación.
+    :rtype: HttpResponse
+    """
     user = Usuario.objects.get(id=user_id)
     page_title = 'Asignar Rol'
     if request.method == 'POST':
@@ -59,6 +93,18 @@ def asignar_rol(request, user_id):
 @login_required
 @has_permission_decorator('asign_roles')
 def desasignar_rol(request,role_category_id):
+    """
+    Vista para desasignar un rol de un usuario.
+
+    Permite a los usuarios con el permiso 'asign_roles' desasignar un rol de un usuario.
+
+    :param request: La solicitud HTTP.
+    :type request: HttpRequest
+    :param role_category_id: El ID del rol de usuario que se va a desasignar.
+    :type role_category_id: int
+    :return: Redirige a la lista de usuarios después de la desasignación.
+    :rtype: HttpResponse
+    """
     role_category = get_object_or_404(UserCategoryRole, id=role_category_id)
     role_category.delete()
     return redirect('usuarios')
