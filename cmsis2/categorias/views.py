09caed596b15bb77fb8c1988e9b09f030042c88a
Jovana <jovananayeli@gmail.com>
2023-09-07 09:42:52 -0400

    Modificacion de templates para categorias

--- cmsis2/categorias/views.py
+++ cmsis2/categorias/views.py
@@ -9,18 +9,24 @@ from django.contrib.auth.decorators import login_required
 @login_required
 @has_permission_decorator('view_category')
 def categorias(request):
+    page_title = 'Categorias'
     list_categorias = Categoria.objects.all().order_by('id')  # Obtén todos los roles
     paginator = Paginator(list_categorias,5)
 
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
 
-    return render(request, 'categorias/categorias.html', {'page': page})
+    return render(request, 'categorias/categorias.html',
+                  {
+                      'page_title': page_title,
+                      'page': page
+                  })
 
 """Crea una categoría"""
 @login_required
 @has_permission_decorator('create_category')
 def crear_categoria(request):
+    page_title = 'Crear Nueva Categoria'
     if request.method == 'POST':
         form = CategoriaForm(request.POST)
         if form.is_valid():
@@ -28,20 +34,32 @@ def crear_categoria(request):
             return redirect('categorias')
     else:
         form = CategoriaForm()
-    return render(request, 'categorias/crear_categoria.html', {'form': form})
+    return render(request, 'categorias/crear_categoria.html',
+                  {
+                      'page_title': page_title,
+                      'form': form
+                  })
 
 """Visualiza el estado de la categoría, cuales son las subcategorías asociadas a la categoría"""
 @login_required
 @has_permission_decorator('view_category')
 def mas_informacion_categoria(request, categoria_id):
     categoria = Categoria.objects.get(id=categoria_id)
+    page_title = categoria.nombre
     subcategorias = Subcategoria.objects.filter(categoria=categoria)
-    return render(request, 'categorias/more_info.html', {'categoria': categoria, 'subcategorias': subcategorias})
+    return render(request, 'categorias/more_info.html',
+                  {
+                      'page_title': page_title,
+                      'categoria': categoria,
+                      'subcategorias': subcategorias
+                  })
 
 """Permite cambiar los detalles de la categpría seleccionada como descripción, inactivar o el nombre"""
 @login_required
 @has_permission_decorator('edit_category')
 def editar_categoria(request, categoria_id):
+    page_title = 'Modificar Categoria'
+
     categoria = get_object_or_404(Categoria, id=categoria_id)
 
     if request.method == 'POST':
@@ -52,7 +70,12 @@ def editar_categoria(request, categoria_id):
     else:
         form = CategoriaForm(instance=categoria)
 
-    return render(request, 'categorias/editar_categoria.html', {'form': form, 'categoria': categoria})
+    return render(request, 'categorias/editar_categoria.html',
+                  {
+                      'page_title': page_title,
+                      'form': form,
+                      'categoria': categoria
+                  })
 
 """Permite inactivar la categoría"""
 @login_required
@@ -68,20 +91,26 @@ def inactivar_categoria(request,categoria_id):
 
 """Crea la vista de subcategorías pertenecientes a una categoría. """
 @login_required
-@has_permission_decorator('edit_category')
+@has_permission_decorator('view_category')
 def subcategorias(request):
+    page_title = 'Subcategorias'
     list_subcategorias = Subcategoria.objects.all().order_by('id')  # Obtén todos los roles
     paginator = Paginator(list_subcategorias,5)
 
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
 
-    return render(request, 'categorias/subcategorias.html', {'page': page})
+    return render(request, 'categorias/subcategorias.html',
+                  {
+                      'page_title': page_title,
+                      'page': page
+                  })
 
 """Crea una subcategoría"""
 @login_required
 @has_permission_decorator('edit_category')
 def crear_subcategoria(request):
+    page_title = 'Crear Nueva Subcategoria'
     if request.method == 'POST':
         form = SubcategoriaForm(request.POST)
         if form.is_valid():
@@ -89,19 +118,29 @@ def crear_subcategoria(request):
             return redirect('subcategorias')  # Redirige a la vista deseada después de crear las subcategorias
     else:
         form = SubcategoriaForm()
-    return render(request, 'categorias/crear_subcategoria.html', {'form': form})
+    return render(request, 'categorias/crear_subcategoria.html',
+                  {
+                      'page_title': page_title,
+                      'form': form
+                  })
 
 """Visualiza el estado de la subcategoría"""
 @login_required
 @has_permission_decorator('edit_category')
 def mas_informacion_subcategoria(request, subcategoria_id):
     subcategoria = Subcategoria.objects.get(id=subcategoria_id)
-    return render(request, 'categorias/more_info_subcategoria.html', {'subcategoria': subcategoria})
+    page_title = subcategoria.nombre
+    return render(request, 'categorias/more_info_subcategoria.html',
+                  {
+                      'page_title': page_title,
+                      'subcategoria': subcategoria
+                  })
 
 """Permite editar el estado de una subcategoría"""
 @login_required
 @has_permission_decorator('edit_category')
 def editar_subcategoria(request, subcategoria_id):
+    page_title = 'Modificar Subcategoria'
     subcategoria = get_object_or_404(Subcategoria, id=subcategoria_id)
 
     if request.method == 'POST':
@@ -112,7 +151,12 @@ def editar_subcategoria(request, subcategoria_id):
     else:
         form = SubcategoriaForm(instance=subcategoria)
 
-    return render(request, 'categorias/editar_subcategoria.html', {'form': form, 'subcategoria': subcategoria})
+    return render(request, 'categorias/editar_subcategoria.html',
+                  {
+                      'page_title': page_title,
+                      'form': form,
+                      'subcategoria': subcategoria
+                  })
 
 """Permite inactivar una subcategoría"""
 @login_required
