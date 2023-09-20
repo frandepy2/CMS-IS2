195d99f6f2e618fb76c93c65521ca266e9cd7bf4
frandepy2 <francisco.sanabria@somosetica.com>
2023-09-19 22:41:40 -0400

    Agrega URL de CKEditor

--- cmsis2/cmsis2/urls.py
+++ cmsis2/cmsis2/urls.py
@@ -17,13 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import HomeView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('socialaccount/', include('allauth.urls')),
+    path(r'^ckeditor/', include('ckeditor_uploader.urls')),
     path('usuarios/',include("usuarios.urls")),
     path('roles/',include("roles.urls")),
     path('categorias/',include("categorias.urls")),
     path('', HomeView, name='home'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
