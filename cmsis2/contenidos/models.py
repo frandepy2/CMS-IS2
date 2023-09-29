479d553ac7e28fcccd5785bb9d42a8082155edb7
frandepy2 <francisco.sanabria@somosetica.com>
2023-09-28 22:34:12 -0400

    Agrega creacion de contenido a partir de plantilla

--- cmsis2/contenidos/models.py
+++ cmsis2/contenidos/models.py
@@ -19,7 +19,7 @@ class Contenido(models.Model):
     autor = models.ForeignKey(Usuario, on_delete=models.PROTECT)
     subcategoria = models.ForeignKey(Subcategoria, on_delete=models.PROTECT, null=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
-    fecha_caducidad = models.DateField()
+    fecha_caducidad = models.DateField(null=True, blank= True)
 
     def __str__(self):
         return self.nombre
