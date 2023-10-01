c0b5426e51151c02bc9dc05526319514482ce2cd
frandepy2 <francisco.sanabria@somosetica.com>
2023-10-01 19:00:13 -0300

    Agrega el panel de control de parametros

--- cmsis2/parametros/models.py
+++ cmsis2/parametros/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-# Create your models here.
+LENGUAJE_COMUN_CHOICES = [
+    ('MAX_CANT_DENUNCIAS', 'MAX_CANT_DENUNCIAS'),
+]
+
 class Parametro(models.Model):
-    clave = models.CharField(max_length=100)
+    clave = models.CharField(max_length=20, choices=LENGUAJE_COMUN_CHOICES, unique=True)
     valor = models.CharField(max_length=255)
 
     def __str__(self):
         return self.clave
-
