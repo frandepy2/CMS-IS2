3bd4f00f19eebf4da4d6c3a712504bd3709926e8
Jovana <jovananayeli@gmail.com>
2023-10-04 22:01:24 -0300

    cambio entrypoint

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -4,11 +4,11 @@
 echo "Inicia el proceso de hacer migraciones"
 python manage.py makemigrations usuarios roles categorias contenidos parametros
 echo "finaliza el proceso de hacer migraciones"
-python manage.py migrate #--noinput
+python manage.py migrate --no-input
 echo "Inicia el proceso de guardar la base de datos"
-#python manage.py shell < load_database.py
+python manage.py shell < load_database.py
 echo "finaliza el proceso de guardar la base de datos"
-python manage.py collectstatic #--noinput
+python manage.py collectstatic --noinput
 
 
 python manage.py runserver 0.0.0.0:8000
