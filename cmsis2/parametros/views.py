1a43859ad4b01d017f7001d415c1421ed28c1834
frandepy2 <francisco.sanabria@somosetica.com>
2023-10-05 21:39:36 -0300

    Agrega roles y permisos a control de parametros

--- cmsis2/parametros/views.py
+++ cmsis2/parametros/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Parametro
+from decorators import has_permission_decorator, has_some_cat_role_decorator
+from django.contrib.auth.decorators import login_required
 
+@login_required
+@has_permission_decorator('manage_parameters')
 def lista_y_editar_parametros(request, parametro_id=None):
     parametros = Parametro.objects.all()
     parametro = None
