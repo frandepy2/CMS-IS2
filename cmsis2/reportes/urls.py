153331007499ad389f7ab5bde8ea34a5724492c7
frandepy2 <francisco.sanabria@somosetica.com>
2023-11-02 18:57:42 -0300

    Crea flujo de reportes por categoria

--- cmsis2/reportes/urls.py
+++ cmsis2/reportes/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('category/dateviews',views.visualizaciones_por_categoria_por_fecha, name='visualizaciones_por_categoria_por_fecha'),
     path('category/datelikes',views.likes_por_categoria_por_fecha, name='likes_por_categoria_por_fecha'),
     path('generar_pdf/', views.generar_pdf, name='generar_pdf'),
+    path('category/<int:contenido_id>/obtener_informacion/', views.get_informacion_contenido, name='get_informacion_contenido')
 ]
