7b85b1896bda30eb4928779646a3ce92bf9abd48
frandepy2 <francisco.sanabria@somosetica.com>
2023-11-25 09:06:07 -0300

    Agrega comentarios de sphinx

--- cmsis2/notificaciones/views.py
+++ cmsis2/notificaciones/views.py
@@ -6,6 +6,13 @@ from usuarios.models import Usuario
 
 # Create your views here.
 def obtener_notificaciones(request, usuario_id):
+    """
+    Obtiene todas las notificaciones para un usuario específico.
+
+    :param request: La solicitud HTTP.
+    :param usuario_id: El ID del usuario para el cual se obtienen las notificaciones.
+    :return: Una respuesta JSON que contiene las notificaciones o un mensaje de error si el usuario no se encuentra.
+    """
     try:
         #Obtenemos todas las notificaciones creadas para el usuario con usuario_id
         receptor = Usuario.objects.get(id=usuario_id)
@@ -33,6 +40,13 @@ def obtener_notificaciones(request, usuario_id):
 
 
 def marcar_como_leido(request, notificacion_id):
+    """
+    Marca una notificación como leída.
+
+    :param request: La solicitud HTTP.
+    :param notificacion_id: El ID de la notificación que se marcará como leída.
+    :return: Una respuesta HTTP indicando que la operación fue exitosa.
+    """
     notificacion = Notificacion.objects.get(id=notificacion_id)
     notificacion.leido = True
     notificacion.save()
